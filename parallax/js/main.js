f221218c40dcd11ddfc1de61cf57b5b6d61cd1b5
AzizulAlam <azizul17bd@gmail.com>
2017-05-13 21:32:53 +0600

    Add files via upload

---
+++ parallax/js/main.js
@@ -0,0 +1,25 @@
+(function($) {
+    "use restrict";
+    
+   jQuery(document).ready(function($){
+    
+      
+        	var s = skrollr.init();
+       
+       $(".video-play-btn").magnificPopup({
+           type: 'video', 
+        });
+       
+       
+       
+   }); 
+    
+    jQuery(window).load(function(){
+    
+       
+        
+    });
+    
+}(jQuery));
+
+ 
