e117028c6fdd2dc38837f872809b9db479eb73de
AzizulAlam <azizul17bd@gmail.com>
2017-05-13 22:56:25 +0600

    Add files via upload

---
+++ home-slider/js/main.js
@@ -0,0 +1,30 @@
+(function($) {
+    "use restrict";
+    
+   jQuery(document).ready(function($){
+    
+//    $(".video-play-btn").magnificPopup({
+//       type:'video', 
+//    });
+    
+       
+   $(".homepage-slides").owlCarousel({
+       items: 1,
+       loop: true,
+       nav: false,
+       dots: true,
+       autoplay: false
+       
+   });
+       
+       
+   }); 
+    
+    jQuery(window).load(function(){
+    
+        //    $().();
+        
+    });
+    
+}(jQuery));
+
