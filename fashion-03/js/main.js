1f8bea2bf4e8227c83a0615760953000b007fed2
AzizulAlam <azizul17bd@gmail.com>
2017-05-17 07:47:06 +0600

    Add files via upload

---
+++ fashion-03/js/main.js
@@ -0,0 +1,16 @@
+//(function($) {
+//    "use restrict";
+//}(jQuery));
+
+jQuery(document).ready(function($){
+    
+    
+//    $().();
+    
+});
+
+jQuery(window).load(function(){
+    
+ //   $().();
+    
+});
